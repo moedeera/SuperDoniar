6c67896541d20e2e65b7d4e617611cac9a3f7485
moedeera <82620264+moedeera@users.noreply.github.com>
2021-09-22 19:30:38 -0600

    slideshow

--- main.js
+++ main.js
@@ -25,3 +25,135 @@ exitBtn.addEventListener('click', (e)=>{
   click = 1;
 
 })
+
+
+// Slider  function 
+const LeftBtn = document.querySelector(".arrow-left")
+const rightBtn = document.querySelector(".arrow-right")
+const circles = document.querySelectorAll(".circles")
+const slide = document.querySelectorAll(".slide")
+var current  = 3;  
+
+
+
+LeftBtn.addEventListener('click', (e)=>{
+
+e.preventDefault();
+MoveLeft();
+
+})
+
+
+rightBtn.addEventListener('click',(e)=>{
+e.preventDefault();
+MoveRight();
+
+})
+
+
+
+function MoveLeft (){
+
+if (current===0){
+
+current = 3;
+var z = 0; 
+
+slide[0].style.left='-300%'
+slide[1].style.left = '-200%'
+slide[2].style.left = '-100%'
+slide[3].style.left = '0'
+} 
+
+
+else if (current===3){
+
+current =2 ;
+
+slide[0].style.left='-200%'
+slide[1].style.left = '-100%'
+slide[2].style.left = '0'
+slide[3].style.left = '100%'
+} 
+
+else if (current===2){
+
+  current =1 ;
+  
+  slide[0].style.left='-100%'
+  slide[1].style.left = '0%'
+  slide[2].style.left = '100%'
+  slide[3].style.left = '200%'
+  } 
+  
+  else if (current===1){
+
+    current =0 ;
+    
+    slide[0].style.left='0%'
+    slide[1].style.left = '100%'
+    slide[2].style.left = '200%'
+    slide[3].style.left = '300%'
+    } 
+    
+
+
+
+
+
+}
+
+
+function MoveRight (){
+
+  if (current===0){
+  
+  current = 1;
+
+  
+  slide[0].style.left='-100%'
+  slide[1].style.left = '0%'
+  slide[2].style.left = '100%'
+  slide[3].style.left = '200%'
+  } 
+  
+  
+  else if (current===1){
+  
+  current =2 ;
+  
+  slide[0].style.left='-200%'
+  slide[1].style.left = '-100%'
+  slide[2].style.left = '0'
+  slide[3].style.left = '100%'
+  } 
+  
+  else if (current===2){
+  
+    current =3 ;
+    
+    slide[0].style.left='-300%'
+    slide[1].style.left = '-200%'
+    slide[2].style.left = '-100%'
+    slide[3].style.left = '0%'
+    } 
+    
+    else if (current===3){
+  
+      current =0 ;
+      
+      slide[0].style.left='0%'
+      slide[1].style.left = '100%'
+      slide[2].style.left = '200%'
+      slide[3].style.left = '300%'
+      } 
+      
+  
+  
+  
+  
+  
+  }
+
+
+
