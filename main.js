1172defe86c1bcc93bac64ee775e1b86702a492e
moedeera <82620264+moedeera@users.noreply.github.com>
2022-04-29 06:28:39 -0600

    committ

--- main.js
+++ main.js
@@ -1,250 +1,212 @@
-// hamberger menu function 
+// hamberger menu function
 
-
-const menu = document.querySelector(".menu")
-const form = document.querySelector(".form-modal")
-const exitBtn = document.getElementById("exit")
+const menu = document.querySelector(".menu");
+const form = document.querySelector(".form-modal");
+const exitBtn = document.getElementById("exit");
 var click = 1;
 
-menu.addEventListener('click', (e)=>{
+menu.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (click === 1) {
+    document.getElementById("form-modal").style.top = "0";
+    click = 2;
+  } else if (click === 2) {
+    document.getElementById("form-modal").style.top = "-100vh";
+    click = 1;
+  }
+});
 
-e.preventDefault()
-if (click === 1){
-document.getElementById("form-modal").style.top ="0"
-click = 2;
-} else if (click === 2 ){
-  document.getElementById("form-modal").style.top ="-100vh" 
+exitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.getElementById("form-modal").style.top = "-100vh";
   click = 1;
-}
+});
 
-})
-
-exitBtn.addEventListener('click', (e)=>{
-  e.preventDefault()
-  document.getElementById("form-modal").style.top ="-100vh" 
+document.getElementById("menuOpt").addEventListener("click", (e) => {
+  e.preventDefault();
+  const element = document.getElementById("menu2");
+  element.scrollIntoView();
+  document.getElementById("form-modal").style.top = "-100vh";
   click = 1;
+});
 
-})
 // Navigation
 
-
 function onLinkClick(x) {
-  if (x===0){
+  if (x === 0) {
     document.getElementById("showcase").scrollIntoView();
- }
-
-  if (x===1){
-     document.getElementById("menu2").scrollIntoView();
   }
- if (x==2){
-  document.getElementById("about").scrollIntoView();
- }
- 
+
+  if (x === 1) {
+    document.getElementById("menu2").scrollIntoView();
+  }
+  if (x == 2) {
+    document.getElementById("about").scrollIntoView();
+  }
 }
 
-document.getElementById("homeOption").addEventListener('click',(e)=>{
-  e.preventDefault()
-  onLinkClick(0)
-})
-document.getElementById("menuOption").addEventListener('click',(e)=>{
-  e.preventDefault()
-  onLinkClick(1)
-})
-document.getElementById("contactOption").addEventListener('click',(e)=>{
-  e.preventDefault()
-  onLinkClick(2)
-})
-document.getElementById("aboutOption").addEventListener('click',(e)=>{
-  e.preventDefault()
-  onLinkClick(2)
-})
+document.getElementById("homeOption").addEventListener("click", (e) => {
+  e.preventDefault();
+  onLinkClick(0);
+});
+document.getElementById("menuOption").addEventListener("click", (e) => {
+  e.preventDefault();
+  onLinkClick(1);
+});
+document.getElementById("contactOption").addEventListener("click", (e) => {
+  e.preventDefault();
+  onLinkClick(2);
+});
+document.getElementById("aboutOption").addEventListener("click", (e) => {
+  e.preventDefault();
+  onLinkClick(2);
+});
 
-// Slider  function 
-const LeftBtn = document.querySelector(".arrow-left")
-const rightBtn = document.querySelector(".arrow-right")
-const circles = [document.getElementById("circle1"),document.getElementById("circle2")
-,document.getElementById("circle3"),document.getElementById("circle4")]
-const slide = document.querySelectorAll(".slide")
-var current  = 0;  
+// Slider  function
+const LeftBtn = document.querySelector(".arrow-left");
+const rightBtn = document.querySelector(".arrow-right");
+const circles = [
+  document.getElementById("circle1"),
+  document.getElementById("circle2"),
+  document.getElementById("circle3"),
+  document.getElementById("circle4"),
+];
+const slide = document.querySelectorAll(".slide");
+var current = 0;
 
+LeftBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  MoveLeft();
+});
 
-
-LeftBtn.addEventListener('click', (e)=>{
-
-e.preventDefault();
-MoveLeft();
-
-
-})
-
-
-rightBtn.addEventListener('click',(e)=>{
-e.preventDefault();
-MoveRight();
-
-
-})
+rightBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  MoveRight();
+});
 
 setInterval(MoveRight, 6000);
 
-function MoveLeft (){
+function MoveLeft() {
+  if (current === 0) {
+    current = 3;
+    var z = 0;
 
-if (current===0){
+    slide[0].style.left = "-300%";
+    slide[1].style.left = "-200%";
+    slide[2].style.left = "-100%";
+    slide[3].style.left = "0";
+  } else if (current === 3) {
+    current = 2;
 
-current = 3;
-var z = 0; 
+    slide[0].style.left = "-200%";
+    slide[1].style.left = "-100%";
+    slide[2].style.left = "0";
+    slide[3].style.left = "100%";
+  } else if (current === 2) {
+    current = 1;
 
-slide[0].style.left='-300%'
-slide[1].style.left = '-200%'
-slide[2].style.left = '-100%'
-slide[3].style.left = '0'
-} 
+    slide[0].style.left = "-100%";
+    slide[1].style.left = "0%";
+    slide[2].style.left = "100%";
+    slide[3].style.left = "200%";
+  } else if (current === 1) {
+    current = 0;
 
-
-else if (current===3){
-
-current =2 ;
-
-slide[0].style.left='-200%'
-slide[1].style.left = '-100%'
-slide[2].style.left = '0'
-slide[3].style.left = '100%'
-} 
-
-else if (current===2){
-
-  current =1 ;
-  
-  slide[0].style.left='-100%'
-  slide[1].style.left = '0%'
-  slide[2].style.left = '100%'
-  slide[3].style.left = '200%'
-  } 
-  
-  else if (current===1){
-
-    current =0 ;
-    
-    slide[0].style.left='0%'
-    slide[1].style.left = '100%'
-    slide[2].style.left = '200%'
-    slide[3].style.left = '300%'
-    } 
-    
-
-    CircleUpdate()
-
-
-
-}
-
-
-function MoveRight (){
-
-  if (current===0){
-  
-  current = 1;
-
-  
-  slide[0].style.left='-100%'
-  slide[1].style.left = '0%'
-  slide[2].style.left = '100%'
-  slide[3].style.left = '200%'
-  } 
-  
-  
-  else if (current===1){
-  
-  current =2 ;
-  
-  slide[0].style.left='-200%'
-  slide[1].style.left = '-100%'
-  slide[2].style.left = '0'
-  slide[3].style.left = '100%'
-  } 
-  
-  else if (current===2){
-  
-    current =3 ;
-    
-    slide[0].style.left='-300%'
-    slide[1].style.left = '-200%'
-    slide[2].style.left = '-100%'
-    slide[3].style.left = '0%'
-    } 
-    
-    else if (current===3){
-  
-      current =0 ;
-      
-      slide[0].style.left='0%'
-      slide[1].style.left = '100%'
-      slide[2].style.left = '200%'
-      slide[3].style.left = '300%'
-      } 
-      
-  
-  
-      CircleUpdate()
-  
-  
+    slide[0].style.left = "0%";
+    slide[1].style.left = "100%";
+    slide[2].style.left = "200%";
+    slide[3].style.left = "300%";
   }
 
-function CircleUpdate(){
-
-for (var j=0; j<circles.length; j++){
-
-  circles[j].classList.remove("chosen");
-if (current===j){
-circles[j].classList.add("chosen")
-}
-}}
-
-
-// Toggle Menu 
-
-
-var MenuTables = [document.getElementById("opt1"),document.getElementById("opt2"),
-document.getElementById("opt3"),document.getElementById("opt4")]
-
-var MenuHeaders = [document.getElementById("opt1h"),document.getElementById("opt2h"),
-document.getElementById("opt3h"),document.getElementById("opt4h")]
-
-var MenuTargets = [document.getElementById("opt1ht"),document.getElementById("opt2ht"),
-document.getElementById("opt3ht"),document.getElementById("opt4ht")]
-
-
-
-
-document.getElementById("choices-section").addEventListener('click', (e)=>{
-e.preventDefault()
-ClearMenus ()   
-
-console.log(e.target)
-
-
-for(var j=0; j<MenuTargets.length; j++ ){
-if (e.target === MenuTargets[j]){
-
-MenuTables[j].style.bottom = "30%"
-MenuHeaders[j].style.top = "10%"
+  CircleUpdate();
 }
 
-}
+function MoveRight() {
+  if (current === 0) {
+    current = 1;
 
+    slide[0].style.left = "-100%";
+    slide[1].style.left = "0%";
+    slide[2].style.left = "100%";
+    slide[3].style.left = "200%";
+  } else if (current === 1) {
+    current = 2;
 
-})
+    slide[0].style.left = "-200%";
+    slide[1].style.left = "-100%";
+    slide[2].style.left = "0";
+    slide[3].style.left = "100%";
+  } else if (current === 2) {
+    current = 3;
 
+    slide[0].style.left = "-300%";
+    slide[1].style.left = "-200%";
+    slide[2].style.left = "-100%";
+    slide[3].style.left = "0%";
+  } else if (current === 3) {
+    current = 0;
 
-
-function ClearMenus () {
-
-
-  for(var j=0; j<MenuTables.length; j++){
-
-    MenuHeaders[j].style.top = "50%"
-  MenuTables[j].style.bottom = "-100%"
-  
+    slide[0].style.left = "0%";
+    slide[1].style.left = "100%";
+    slide[2].style.left = "200%";
+    slide[3].style.left = "300%";
   }
-  
+
+  CircleUpdate();
+}
+
+function CircleUpdate() {
+  for (var j = 0; j < circles.length; j++) {
+    circles[j].classList.remove("chosen");
+    if (current === j) {
+      circles[j].classList.add("chosen");
+    }
+  }
+}
+
+// Toggle Menu
+
+var MenuTables = [
+  document.getElementById("opt1"),
+  document.getElementById("opt2"),
+  document.getElementById("opt3"),
+  document.getElementById("opt4"),
+];
+
+var MenuHeaders = [
+  document.getElementById("opt1h"),
+  document.getElementById("opt2h"),
+  document.getElementById("opt3h"),
+  document.getElementById("opt4h"),
+];
+
+var MenuTargets = [
+  document.getElementById("opt1ht"),
+  document.getElementById("opt2ht"),
+  document.getElementById("opt3ht"),
+  document.getElementById("opt4ht"),
+];
+
+document.getElementById("choices-section").addEventListener("click", (e) => {
+  e.preventDefault();
+  //
+
+  for (var j = 0; j < MenuTargets.length; j++) {
+    if (e.target === MenuTargets[j]) {
+      if (MenuTables[j].style.bottom === "30%") {
+        ClearMenus();
+      } else {
+        ClearMenus();
+        MenuTables[j].style.bottom = "30%";
+        MenuHeaders[j].style.top = "10%";
+      }
+    }
+  }
+});
+
+function ClearMenus() {
+  for (var j = 0; j < MenuTables.length; j++) {
+    MenuHeaders[j].style.top = "50%";
+    MenuTables[j].style.bottom = "-100%";
+  }
 }
